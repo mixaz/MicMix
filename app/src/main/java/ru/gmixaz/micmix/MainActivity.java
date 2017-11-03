0cf16edb2529a88c1bc631a12712cb0d61256de2
Mikhail Zemlyanukha <gmixaz@gmail.com>
2017-11-03 13:48:12 +0300

    minor UI changes, remove toolbar

--- app/src/main/java/ru/gmixaz/micmix/MainActivity.java
+++ app/src/main/java/ru/gmixaz/micmix/MainActivity.java
@@ -33,9 +33,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_main);
 
         pref = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
 
@@ -82,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
                         copyFile("mixer_paths.xml", "%DEC1_VOL%", ss, "/data/data/" + getPackageName() + "/");
                         execCommand("cp /data/data/" + getPackageName() + "/mixer_paths.xml /system/etc/");
                         text.setText("Current value: " + ss);
+                        Toast.makeText(MainActivity.this,"command executed OK",Toast.LENGTH_SHORT).show();
                     }
                     else {
                         text.setText("Mount system RW first!");
@@ -108,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
                         copyFile("mixer_paths.xml", "%DEC1_VOL%", ss, "/data/data/" + getPackageName() + "/");
                         execCommand("cp /data/data/" + getPackageName() + "/mixer_paths.xml /system/etc/");
                         text.setText("Current value: "+ss);
+                        Toast.makeText(MainActivity.this,"command executed OK",Toast.LENGTH_SHORT).show();
                     }
                     else {
                         text.setText("Mount system RW first!");
@@ -179,25 +179,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
